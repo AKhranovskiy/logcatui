c6dbcb312dc2da80896320ded0d264f03ea4063b
Andrey Khranovsky <akhranovskiy@gmail.com>
2021-07-24 18:20:33 +0200

    Update color scheme

--- src/styles.rs
+++ src/styles.rs
@@ -11,9 +11,19 @@ lazy_static! {
         .add_modifier(Modifier::BOLD);
     pub static ref STYLE_SEARCH_HIGHLIGHT: Style =
         Style::default().fg(Color::Yellow).bg(Color::Blue);
-    pub static ref STYLE_LOGLEVEL_ERROR: Style = Style::default().bg(Color::Red);
-    pub static ref STYLE_LOGLEVEL_INFO: Style = Style::default().bg(Color::LightRed);
-    pub static ref STYLE_LOGLEVEL_WARNING: Style = Style::default().bg(Color::Magenta);
-    pub static ref STYLE_LOGLEVEL_DEBUG: Style = Style::default().bg(Color::Cyan);
-    pub static ref STYLE_LOGLEVEL_VERBOSE: Style = Style::default().bg(Color::Gray);
+    pub static ref STYLE_LOGLEVEL_ERROR: Style = Style::default()
+        .bg(Color::Rgb(0xF4, 0x43, 0x36))
+        .fg(Color::Black);
+    pub static ref STYLE_LOGLEVEL_INFO: Style = Style::default()
+        .bg(Color::Rgb(0x4C, 0xAF, 0x50))
+        .fg(Color::Black);
+    pub static ref STYLE_LOGLEVEL_WARNING: Style = Style::default()
+        .bg(Color::Rgb(0xFF, 0xC1, 0x07))
+        .fg(Color::Black);
+    pub static ref STYLE_LOGLEVEL_DEBUG: Style = Style::default()
+        .bg(Color::Rgb(0x21, 0x96, 0xF3))
+        .fg(Color::Black);
+    pub static ref STYLE_LOGLEVEL_VERBOSE: Style = Style::default()
+        .bg(Color::Rgb(0x9C, 0x27, 0xB0))
+        .fg(Color::Black);
 }
